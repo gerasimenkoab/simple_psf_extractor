9063060991d1d88c4066d26dfd60fd0e389e1caf
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-07-25 23:22:16 +0200

    cnn-work-added

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from controller.extractor_controller import ExtractorController
 from controller.decon_controller import DeconController
+from cnn.cnn_deconvolution_gui import *
 import logging
 import logging.config
 import os
@@ -37,7 +38,7 @@ class main_window_gui(tk.Tk):
     
     def OpenNNDeconvolution(self):
         """Loadding NN deconvolution widget window"""
-        pass
+        deconvolver = CNNDeconvGUI(self)
 
     def Authors(self):
         """Loadding Authors List widget window"""
